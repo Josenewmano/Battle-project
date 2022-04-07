13c5eae50b85b5e7a75485f34f206035f860733b
Joe <j.newman@live.co.uk>
2022-04-07 14:42:57 +0100

    removed player global variables, added game global variable

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,17 @@
 require_relative '../lib/game'
 
 describe Game do
-  let(:fred) { double :player }
-  let(:george) { double :player }
-  game = Game.new
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
 
+  it 'does not throw an error when initialised with two players' do
+    game = Game.new(player_1, player_2)
+    expect(game.player_1).to eq(player_1)
+  end
+  
   it 'causes damage to the player when the attack method is called' do
-    expect(fred).to receive(:receive_damage)
-    subject.attack(fred)
+    game = Game.new(player_1, player_2)
+    expect(game.player_1).to receive(:receive_damage)
+    game.attack(player_1)
   end
 end
